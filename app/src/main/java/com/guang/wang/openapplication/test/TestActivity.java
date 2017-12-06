d4ef5386fe683c4d1b1b831c0a5188c63c680e17
wangguang <wangguang03@58ganji.com>
2017-12-06 17:34:09 +0800

    王广：生成module

---
+++ app/src/main/java/com/guang/wang/openapplication/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.guang.wang.openapplication.test;
+
+import com.guang.wang.openapplication.BaseActivity;
+import com.guang.wang.openapplication.R;
+
+import android.os.Bundle;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
